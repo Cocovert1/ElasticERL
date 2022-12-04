858e113dd30f711fe43b307c60a7e9f30d15c153
Greatone3145 <alessiock@outlook.com>
2022-12-04 14:17:51 -0500

    New BinarySearchTree

---
+++ src/BinarySearchTree.java
@@ -0,0 +1,109 @@
+public class BinarySearchTree {
+    //Node class to be used by BinarySearchTree, stores values for the tree to hold
+    static class Node {
+        public String key;
+        public Node left;
+        public Node right;
+
+        public Node(String key){
+            this.key = key;
+            left = null;
+            right = null;
+        }
+
+        public String toString(){
+            return this.key;
+        }
+    }
+
+    //Starting point of the tree
+    public Node root;
+
+    //Constructor to build empty BinarySearchTree;
+    public BinarySearchTree(){
+        root = null;
+    }
+
+    public String search(String key){
+        root = searchRecursive(root, key);
+        if(root != null){
+            return key;
+        }else{
+            return "Key not found";
+        }
+    }
+
+    public Node searchRecursive(Node root, String key){
+        if(root == null || root.key.equals(key)){
+            return root;
+        }
+        if(root.key.compareTo(key) > 0){
+            return searchRecursive(root.left, key);
+        }
+        return searchRecursive(root.right, key);
+    }
+
+    public void insert(String key){
+        root = insertRecursive(root, key);
+    }
+
+    //Insert recursively, checking for values
+    public Node insertRecursive(Node root, String key){
+        if(root == null){
+            root = new Node(key);
+            return root;
+        }
+        if(key.compareTo(root.key) < 0){
+            root.left = insertRecursive(root.left, key);
+        }else if(key.compareTo(root.key) > 0){
+            root.right = insertRecursive(root.right, key);
+        }
+        return root;
+    }
+
+    public void remove(String key){
+        root = removeRecursive(root, key);
+    }
+
+    public Node removeRecursive(Node root, String key){
+        if(root == null){
+            return root;
+        }
+        if(key.compareTo(root.key) < 0){
+            root.left = removeRecursive(root.left, key);
+        }else if(key.compareTo(root.key) > 0){
+            root.right = removeRecursive(root.right, key);
+        }else{ //1 Child
+            if(root.left == null){
+                return root.right;
+            }else if(root.right == null){
+                return root.left;
+            }
+
+            root.key = min(root.right);
+            root.right = removeRecursive(root.right, root.key);
+        }
+        return root;
+    }
+
+    public String min(Node root){
+        String minv = root.key;
+        while(root.left != null){
+            minv = root.left.key;
+            root = root.left;
+        }
+        return minv;
+    }
+
+    public void inorder(){
+        inorderRecursive(root);
+    }
+
+    public void inorderRecursive(Node root){
+        if(root != null){
+            inorderRecursive(root.left);
+            System.out.print(root.key + " ");
+            inorderRecursive(root.right);
+        }
+    }
+}
--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,158 +1,160 @@
-public class LinkedList {
-    //node class
-    class Node {
-        int val;
-        Node next;
-
-        //node constructor
-        Node(int val){
-            this.val = val;
-        }
-    }
-
-    //creating our node objects
-    private Node header;
-    private Node trailer;
-
-    //make the new node, add head to next
-    public void addFirst(int val){
-        if(isEmpty()) {
-            header = new Node(val);
-            trailer = header;
-            return;
-        }
-
-        Node newNode = new Node(val);
-        newNode.next = header;
-        header = newNode;
-    }
-
-    //make the new node, add the node to the next of the tail node
-    public void addLast(int val){
-        if(isEmpty()) {
-            header = new Node(val);
-            trailer = header;
-            return;
-        }
-
-        Node newNode = new Node(val);
-        trailer.next = newNode;
-        trailer = newNode;
-    }
-
-    //move to the position before where we want to insert, and fix the pointers
-    public void add(int pos, int val){
-        //validation
-        if( pos == 1 ){
-            addFirst(val);
-            return;
-        }
-
-        Node current = header;
-        int count = 1;
-
-        while(count < pos-1) {
-            count++;
-            current = current.next;
-        }
-
-        Node newNode = new Node(val);
-        newNode.next = current.next;
-        current.next = newNode;
-    }
-
-    //make the header point to the next and remove all the pointers the first had
-    public int deleteFirst(){
-        if(isEmpty()) {
-            throw new RuntimeException("List is Empty");
-        }
-
-        Node current = header;
-        header = header.next;
-        current.next = null;
-
-        if(isEmpty()) {
-            trailer = null;
-        }
-
-        return current.val;
-    }
-
-    //move to the before last node and last node and remove the pointers
-    public int deleteLast() {
-        if(isEmpty()) {
-            throw new RuntimeException("List is Empty");
-        }
-
-        if(header == trailer) {
-            int val = header.val;
-            header = null;
-            trailer = null;
-            return val;
-        }
-
-        Node current = header;
-        Node prev = null;
-
-        while (current.next != null){
-            prev = current;
-            current = current.next;
-        }
-
-        prev.next = null;
-        trailer = prev;
-        return current.val;
-    }
-
-    //move to the position just before, and jump the pointer to the current.next
-    public int delete(int pos){
-        if(isEmpty()) {
-            throw new RuntimeException("List is Empty");
-        }
-
-        if(pos == 1) {
-            deleteFirst();
-        }
-
-        Node current = header;
-        Node previous = null;
-        int count = 1;
-
-        while(count < pos) {
-            count++;
-            previous = current;
-            current = current.next;
-        }
-
-        previous.next = current.next;
-        current.next = null;
-        return current.val;
-    }
-
-    //searches for a value in the linked list
-    public boolean search(int val){
-        Node current = header;
-        while(current != null){
-            if(val == current.val) {
-                return true;
-            }
-            current = current.next;
-        }
-        return false;
-    }
-
-    public boolean isEmpty(){
-        return header == null ;
-    }
-
-
-    //prints the contents of the linked list
-    public void print(){
-        Node current = header;
-        while(current != null){
-            System.out.print(current.val +" -> ");
-            current = current.next;
-        }
-        System.out.println("NULL");
-    }
-}
+//public class LinkedList {
+//    //node class
+//    class Node {
+//        String key;
+//        Node left, right;
+//
+//        //node constructor
+//        Node(String key){
+//            this.key = key;
+//            left = null;
+//            right = null;
+//        }
+//    }
+//
+//    //creating our node objects
+//    private Node header;
+//    private Node trailer;
+//
+//    //make the new node, add head to next
+//    public void addFirst(String key){
+//        if(isEmpty()) {
+//            header = new Node(key);
+//            trailer = header;
+//            return;
+//        }
+//
+//        Node newNode = new Node(key);
+//        newNode.next = header;
+//        header = newNode;
+//    }
+//
+//    //make the new node, add the node to the next of the tail node
+//    public void addLast(String key){
+//        if(isEmpty()) {
+//            header = new Node(key);
+//            trailer = header;
+//            return;
+//        }
+//
+//        Node newNode = new Node(key);
+//        trailer.next = newNode;
+//        trailer = newNode;
+//    }
+//
+//    //move to the position before where we want to insert, and fix the pointers
+//    public void add(int pos, String key){
+//        //validation
+//        if( pos == 1 ){
+//            addFirst(key);
+//            return;
+//        }
+//
+//        Node current = header;
+//        int count = 1;
+//
+//        while(count < pos-1) {
+//            count++;
+//            current = current.next;
+//        }
+//
+//        Node newNode = new Node(key);
+//        newNode.next = current.next;
+//        current.next = newNode;
+//    }
+//
+//    //make the header point to the next and remove all the pointers the first had
+//    public String deleteFirst(){
+//        if(isEmpty()) {
+//            throw new RuntimeException("List is Empty");
+//        }
+//
+//        Node current = header;
+//        header = header.next;
+//        current.next = null;
+//
+//        if(isEmpty()) {
+//            trailer = null;
+//        }
+//
+//        return current.key;
+//    }
+//
+//    //move to the before last node and last node and remove the pointers
+//    public String deleteLast() {
+//        if(isEmpty()) {
+//            throw new RuntimeException("List is Empty");
+//        }
+//
+//        if(header == trailer) {
+//            String key = header.key;
+//            header = null;
+//            trailer = null;
+//            return key;
+//        }
+//
+//        Node current = header;
+//        Node prev = null;
+//
+//        while (current.next != null){
+//            prev = current;
+//            current = current.next;
+//        }
+//
+//        prev.next = null;
+//        trailer = prev;
+//        return current.key;
+//    }
+//
+//    //move to the position just before, and jump the pointer to the current.next
+//    public String delete(int pos){
+//        if(isEmpty()) {
+//            throw new RuntimeException("List is Empty");
+//        }
+//
+//        if(pos == 1) {
+//            deleteFirst();
+//        }
+//
+//        Node current = header;
+//        Node previous = null;
+//        int count = 1;
+//
+//        while(count < pos) {
+//            count++;
+//            previous = current;
+//            current = current.next;
+//        }
+//
+//        previous.next = current.next;
+//        current.next = null;
+//        return current.key;
+//    }
+//
+//    //searches for a value in the linked list
+//    public boolean search(String key){
+//        Node current = header;
+//        while(current != null){
+//            if(key.equals(current.key)) {
+//                return true;
+//            }
+//            current = current.next;
+//        }
+//        return false;
+//    }
+//
+//    public boolean isEmpty(){
+//        return header == null ;
+//    }
+//
+//
+//    //prints the contents of the linked list
+//    public void print(){
+//        Node current = header;
+//        while(current != null){
+//            System.out.print(current.key +" -> ");
+//            current = current.next;
+//        }
+//        System.out.println("NULL");
+//    }
+//}
